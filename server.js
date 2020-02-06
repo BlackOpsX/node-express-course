df0d7228c3726a97c35e12afc698e1dfb8bc6d20
Destin <destinodjedjoh@gmail.com>
2020-02-06 10:49:50 +0100

    add first get route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Destin'},
+    {name: 'Djedjoh'},
+    {name: 'Godwe'}
+    ]
+app.get('/users', function(req,res){
+        res;JSON({
+            success: true,
+            message: 'successfully got users. Nice!',
+            users: mockUserData
+        })
+    })
 app.listen(8000, function(){
     console.log("server is running");
     
